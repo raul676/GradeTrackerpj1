6e73d5274ba0368f518cbecededf8424f78ff5d3
Emory Schmitt <emory.m.schmitt@gmail.com>
2020-09-05 11:51:53 -0700

    Layout For DeleteCourse

--- app/src/main/java/com/example/gradetracker_pj1/DeleteCourse.java
+++ app/src/main/java/com/example/gradetracker_pj1/DeleteCourse.java
@@ -1,6 +1,22 @@
 package com.example.gradetracker_pj1;
 
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 //User types in course num then can delete it, confirmation screen shows then takes back to main activity
-public class DeleteCourse {
+public class DeleteCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deletecourse_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
 }
