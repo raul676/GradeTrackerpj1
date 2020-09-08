bcf7a7e5d6d853ac972dc79b1d0ca2b857a7c880
Emory Schmitt <emory.m.schmitt@gmail.com>
2020-09-08 01:18:40 -0700

    Added Edit Course layout and code, set up delete and add layouts no java code for delete/add

--- app/src/main/java/com/example/gradetracker_pj1/DeleteCourse.java
+++ app/src/main/java/com/example/gradetracker_pj1/DeleteCourse.java
@@ -1,22 +1,14 @@
 package com.example.gradetracker_pj1;
 
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-//User types in course num then can delete it, confirmation screen shows then takes back to main activity
 public class DeleteCourse extends AppCompatActivity {
-
-    @Override
-    protected void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.deletecourse_activity);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-    }
 
 }
