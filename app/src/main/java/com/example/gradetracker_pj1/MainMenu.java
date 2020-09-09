f705cde09e073bdb21c773bbec68e092872c8e42
Emory Schmitt <emory.m.schmitt@gmail.com>
2020-09-09 01:08:45 -0700

    Menu buttons all done

--- app/src/main/java/com/example/gradetracker_pj1/MainMenu.java
+++ app/src/main/java/com/example/gradetracker_pj1/MainMenu.java
@@ -1,7 +1,6 @@
 package com.example.gradetracker_pj1;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,8 +9,6 @@ import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
-import android.widget.TextView;
-
 
 
 public class MainMenu extends AppCompatActivity {
@@ -23,13 +20,40 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("MainMenu", "onCreate called");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
-        
+        setContentView(R.layout.menumain_activity);
 
+        /**
+         * There is a lot of buttons here: please use them to check if your class loads up (all
+         * buttons are set to editcourse) we can always get rid of a lot of these buttons. Also
+         * the Back button needs to be changed to the log out button.
+         */
         Button edit_button = findViewById(R.id.edit_button);
         Button back_button = findViewById(R.id.back_button);
-        
+        Button assignment_button = findViewById(R.id.assignment_button);
+        Button assignmentEdit_button = findViewById(R.id.assignmentEdit_button);
+        Button gradesView_button = findViewById(R.id.gradesView_button);
+        Button courseView_button = findViewById(R.id.courseView_button);
+        Button course_button = findViewById(R.id.courseView_button);
 
+        //enrollment
+        course_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, EditCourse.class);
+                startActivity(intent);
+            }
+        });
+
+        //view courses
+        courseView_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, EditCourse.class);
+                startActivity(intent);
+            }
+        });
+
+        //edit courses
        edit_button.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
@@ -38,6 +62,35 @@ public class MainMenu extends AppCompatActivity {
            }
        });
 
+       //views assignments
+        assignment_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, EditCourse.class);
+                startActivity(intent);
+            }
+        });
+
+        //edit assignments
+        assignmentEdit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, EditCourse.class);
+                startActivity(intent);
+            }
+        });
+
+        //view grades
+        gradesView_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this, EditCourse.class);
+                startActivity(intent);
+            }
+        });
+
+
+      //change this to logout
       back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
