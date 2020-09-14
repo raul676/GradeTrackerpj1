4d32cf29d9e1b9dd95bfde223e93c67f9d513621
Emory Schmitt <emory.m.schmitt@gmail.com>
2020-09-14 12:33:28 -0700

    delete course

--- app/src/main/java/com/example/gradetracker_pj1/DeleteCourse.java
+++ app/src/main/java/com/example/gradetracker_pj1/DeleteCourse.java
@@ -16,6 +16,8 @@ import androidx.room.Delete;
 import com.example.gradetracker_pj1.model.Course;
 import com.example.gradetracker_pj1.model.GradeDao;
 import com.example.gradetracker_pj1.model.GradeRoom;
+import com.example.gradetracker_pj1.model.User;
+
 
 import java.util.List;
 
@@ -39,21 +41,15 @@ public class DeleteCourse extends AppCompatActivity {
                 Log.d("DeleteCourse", "Enter the Course id");
 
                 EditText id = findViewById(R.id.courseID);
-                String course_id = id.getText().toString();
-
-               // int course_id = Integer.parseInt(courseID);
+                String course = id.getText().toString();
+                int course_id =Integer.parseInt(course);
 
                 GradeDao dao = GradeRoom.getGradeRoom(DeleteCourse.this).dao();
-               // Course course = dao.searchCourse(course_id);
+                courses = GradeRoom.getGradeRoom(DeleteCourse.this).dao().getAllCourses();
+                Course course1 = dao.searchCourse(course_id);
 
-                //Loop to find course id
-              /*  for (int i = 0; i < courses.size(); i++) {
-                    if (courses.get(course_id).getCourse_id() == course_id) {
-                    } else {
-                        courses.remove(i);
-                    }
-                }*/
-                if (course_id != null) {
+                if (course != null) {
+
                     AlertDialog.Builder builder = new AlertDialog.Builder(DeleteCourse.this);
                     builder.setTitle("This course will be deleted ");
                     builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
@@ -61,6 +57,7 @@ public class DeleteCourse extends AppCompatActivity {
                         public void onClick(DialogInterface dialog, int which) {
                             //deleting
                          GradeDao dao = GradeRoom.getGradeRoom(DeleteCourse.this).dao();
+                            dao.deleteCourse(course1);
                             Log.d("DeleteCourse", "deletingcourse");
                             Intent intent = new Intent(DeleteCourse.this, MainMenu.class);
                             startActivity(intent);
