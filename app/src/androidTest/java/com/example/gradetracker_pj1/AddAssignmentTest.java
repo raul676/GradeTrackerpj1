717a139d07be74d2bc56de6024e9084bd62e762f
Athena Raya <atraya@csumb.edu>
2020-09-17 12:39:35 -0700

    additonal testcase to DB

--- app/src/androidTest/java/com/example/gradetracker_pj1/AddAssignmentTest.java
+++ app/src/androidTest/java/com/example/gradetracker_pj1/AddAssignmentTest.java
@@ -1,12 +1,28 @@
 package com.example.gradetracker_pj1;
 
-import com.example.gradetracker_pj1.model.Assignment;
+import android.content.Context;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.gradetracker_pj1.model.Assignment;
+import com.example.gradetracker_pj1.model.Course;
+import com.example.gradetracker_pj1.model.GradeDao;
+import com.example.gradetracker_pj1.model.GradeRoom;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class AddAssignmentTest {
+
+    private GradeDao dao;
+
 
     /** A unit test to test add assignment by checking the values of the new assignment by
      * asserting the values are equal
@@ -23,6 +39,33 @@ public class AddAssignmentTest {
         assertEquals("details", assignment.getDetails());
         assertEquals("01/01/2021", assignment.getAssigned_date());
         assertEquals("01/06/2021", assignment.getDue_date());
+
+    }
+    /** on create test, gets the context (database) required to do test  */
+    @Before
+    public void createDB(){
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        GradeDao dao = GradeRoom.getGradeRoom(appContext).dao();
+        assertEquals("com.example.gradetracker_pj1", appContext.getPackageName());
+    }
+
+    /** testing on click when deletion occurs by adding an course to a list then deleting course and checking if the
+     * list is empty
+     */
+    @Test
+    public void onClick() {
+        Context AddAssignment = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        List<Assignment> assignmentList = GradeRoom.getGradeRoom(AddAssignment).dao().getAllAssignments();
+
+        Assignment assignment1 = new Assignment(0001,0002,0003,100,"details","01/01/2021", "01/06/2021");
+
+        assignmentList.add(assignment1);
+        assertNotNull(assignment1);
+
+        GradeDao dao = GradeRoom.getGradeRoom(AddAssignment).dao();
+        dao.addAssignment(assignment1);
+
+        assertTrue(!assignmentList.isEmpty());
 
     }
 }
--- app/src/androidTest/java/com/example/gradetracker_pj1/AddCourseTest.java
+++ app/src/androidTest/java/com/example/gradetracker_pj1/AddCourseTest.java
@@ -1,12 +1,27 @@
 package com.example.gradetracker_pj1;
 
-import com.example.gradetracker_pj1.model.Course;
+import android.content.Context;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.gradetracker_pj1.model.Assignment;
+import com.example.gradetracker_pj1.model.Course;
+import com.example.gradetracker_pj1.model.GradeDao;
+import com.example.gradetracker_pj1.model.GradeRoom;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class AddCourseTest {
+
+    private GradeDao dao;
     /** A unit test to test add course by checking the values of the new course by
      * asserting the values are equal
      */
@@ -22,5 +37,34 @@ public class AddCourseTest {
         assertEquals("06/01/2021", course.getEnd_date());
 
     }
+
+    /** on create test, gets the context (database) required to do test  */
+    @Before
+    public void createDB(){
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        GradeDao dao = GradeRoom.getGradeRoom(appContext).dao();
+        assertEquals("com.example.gradetracker_pj1", appContext.getPackageName());
+    }
+
+    /** testing on click when deletion occurs by adding an course to a list then deleting course and checking if the
+     * list is empty
+     */
+    @Test
+    public void onClick() {
+        Context AddCourse = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        List<Course> courseList = GradeRoom.getGradeRoom(AddCourse).dao().getAllCourses();
+
+        Course course1 = new Course(383, "Dr. Bruns", "Into to Data Science", "Data", "8/24/20", "12/16/20");
+
+        courseList.add(course1);
+        assertNotNull(course1);
+
+        GradeDao dao = GradeRoom.getGradeRoom(AddCourse).dao();
+        dao.addCourse(course1);
+
+        assertTrue(!courseList.isEmpty());
+
+    }
+
 }
 
